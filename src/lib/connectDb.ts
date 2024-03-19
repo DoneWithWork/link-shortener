a8e7b07e5b38b659a5d93c5bed38b63ee57bfa95
DoneWithWork <fingfingsavage@gmail.com>
2024-03-19 23:02:17 +0800

    types

---
+++ src/lib/connectDb.ts
@@ -0,0 +1,47 @@
+import mongoose from 'mongoose';
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      mongoose: any;
+    }
+  }
+}
+
+if (!process.env.DATABASE_URL) {
+  throw new Error(
+    "Please define the DATABASE_URL environment variable inside .env.local"
+  );
+}
+
+let cached = global.mongoose;
+
+declare global {
+  var mongoose: any;
+}
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function dbConnect() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      bufferCommands: false,
+    };
+
+    cached.promise = mongoose.connect(process.env.DATABASE_URL, opts).then((mongoose) => {
+      return mongoose;
+    });
+  }
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
+
+export default dbConnect;
